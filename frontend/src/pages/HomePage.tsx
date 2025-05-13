58273f6441fa5e667908715d78819153acf6fc05
Rinnosuke Nomoto <rin.n.b.1351206@gmail.com>
2025-05-14 01:43:19 +0900

    feat: HomePageに引き出しを追加

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -5,8 +5,21 @@ import {
     AppBar,
     Toolbar,
     Button,
+    Drawer,
+    Divider,
+    Box,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
 } from '@mui/material';
+import InboxIcon from '@mui/icons-material/Inbox';
+import MailIcon from '@mui/icons-material/Mail';
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 import { logout } from '../services/loginService';
+
+const drawerWidth = 240;
 
 const HomePage: React.FC = () => {
     const navigate = useNavigate();
@@ -21,8 +34,8 @@ const HomePage: React.FC = () => {
     };
 
     return (
-        <div className="Home-container">
-            <AppBar position="static">
+        <Box sx={{ display: 'flex' }}>
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         野本の勉強用
@@ -43,7 +56,37 @@ const HomePage: React.FC = () => {
                     </Button>
                 </Toolbar>
             </AppBar>
-        </div>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                        marginTop: '64px', // AppBarの高さ分のマージン
+                    },
+                }}
+            >
+                <Box sx={{ overflow: 'auto' }}>
+                    <List>
+                        {['Todoリスト'].map((text) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <CheckRoundedIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: '64px' }}>
+                {/* メインコンテンツをここに配置 */}
+            </Box>
+        </Box>
     );
 };
 
