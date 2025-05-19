26c109d90b380124faeba982deffa32ce4eaec05
Rinnosuke Nomoto <rin.n.b.1351206@gmail.com>
2025-05-19 23:50:10 +0900

    feat: ホーム画面にメニューアイコン追加

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,28 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
     Typography,
     AppBar,
     Toolbar,
     Button,
-    Drawer,
-    Divider,
     Box,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
+    IconButton,
+    Avatar,
 } from '@mui/material';
-import InboxIcon from '@mui/icons-material/Inbox';
-import MailIcon from '@mui/icons-material/Mail';
-import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+import DomainVerificationRoundedIcon from '@mui/icons-material/DomainVerificationRounded';
+import FitnessCenterRoundedIcon from '@mui/icons-material/FitnessCenterRounded';
 import { logout } from '../services/loginService';
 
 const drawerWidth = 240;
 
 const HomePage: React.FC = () => {
     const navigate = useNavigate();
+    const [message, setMessage] = useState<string>('');
 
     const handleLogout = async () => {
         const response = await logout();
@@ -33,60 +28,64 @@ const HomePage: React.FC = () => {
         }
     };
 
+    const handleWorkoutClick = () => {
+        setMessage('筋トレボタン押されました');
+    };
+
     return (
-        <Box sx={{ display: 'flex' }}>
-            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        野本の勉強用
-                    </Typography>
-                    <Button
-                        color="inherit"
-                        variant="outlined"
-                        onClick={handleLogout}
-                        sx={{
-                            '&:hover': {
-                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                                transform: 'scale(1.05)',
-                                transition: 'all 0.3s ease-in-out'
-                            }
-                        }}
-                    >
-                        ログアウト
-                    </Button>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                variant="permanent"
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    [`& .MuiDrawer-paper`]: {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                        marginTop: '64px', // AppBarの高さ分のマージン
-                    },
-                }}
-            >
-                <Box sx={{ overflow: 'auto' }}>
-                    <List>
-                        {['Todoリスト'].map((text) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <CheckRoundedIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary={text} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Box>
-            </Drawer>
-            <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: '64px' }}>
-                {/* メインコンテンツをここに配置 */}
+        <>
+            <Box sx={{ display: 'flex' }}>
+                <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            野本の勉強用
+                        </Typography>
+                        <Button
+                            color="inherit"
+                            variant="outlined"
+                            onClick={handleLogout}
+                            sx={{
+                                '&:hover': {
+                                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                                    transform: 'scale(1.05)',
+                                    transition: 'all 0.3s ease-in-out'
+                                }
+                            }}
+                        >
+                            ログアウト
+                        </Button>
+                    </Toolbar>
+                </AppBar>
             </Box>
-        </Box>
+            <Box sx={{
+                marginTop: '200px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: 6
+            }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <IconButton>
+                        <Avatar sx={{ bgcolor: 'primary.main', width: 200, height: 200 }}>
+                            <DomainVerificationRoundedIcon sx={{ fontSize: '160px', color: '#fff' }} />
+                        </Avatar>
+                    </IconButton>
+                    <Typography variant="h6" sx={{ mt: 2, fontSize: '30px' }}>
+                        Todo
+                    </Typography>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <IconButton>
+                        <Avatar sx={{ bgcolor: 'primary.main', width: 200, height: 200 }}>
+                            <FitnessCenterRoundedIcon sx={{ fontSize: '160px', color: '#fff' }} />
+                        </Avatar>
+                    </IconButton>
+                    <Typography variant="h6" sx={{ mt: 2, fontSize: '30px' }}>
+                        筋トレ
+                    </Typography>
+                </Box>
+            </Box>
+        </>
     );
 };
 
